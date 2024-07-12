75939e9b570aba5285192eca0e83673187d1a762
aryan <comauro7511@gmail.com>
2024-07-12 06:34:39 +0530

    minor change

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { Toaster } from '@/components/ui/toaster';
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: 'pagementor',
+  title: 'PageMentor',
   description: 'Ai assitance in page writing'
 };
 
